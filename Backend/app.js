7374940544f5bcbaa04a06bd26fa5368d56894b9
Sultan2403 <sultanremilekun@gmail.com>
2026-04-15 17:27:17 +0100

    basic scaffolding and stuff

---
+++ Backend/app.js
@@ -0,0 +1,40 @@
+// Main
+const express = require("express");
+const app = express();
+const connectDB = require("./DB/Connections/connectDB");
+
+// Middlewares
+const { errors } = require("celebrate");
+const cors = require("cors");
+
+connectDB();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://sultan2403.github.io",
+      "https://idea-dump-pro.vercel.app",
+    ],
+  }),
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: " Looking for something? Well it's not here XD" });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ success: true, message: "Server says heyyy :)" });
+});
+
+// Routes
+app.use("/ideas", ideasRouter);
+app.use("/auth", authRouter);
+app.use("/speech", audioRouter);
+
+app.use(errors());
+
+module.exports = app;
