d03466c28e96934b81335a2582a673d7cc19d5af
Sultan2403 <sultanremilekun@gmail.com>
2026-05-12 04:15:43 +0100

    stuff

--- Backend/src/DB/Connections/redis.ts
+++ Backend/src/DB/Connections/redis.ts
@@ -7,17 +7,18 @@ const redisOptions = {
   maxRetriesPerRequest: null, // Required by BullMQ
 };
 
-// 1. Connection for your manual app logic (Caching, etc.)
-export const redis = new Redis(redisOptions);
+const redis = new Redis(redisOptions);
 
-redis.on("connect", () => {
-  console.log("✅ App redis connection ready");
-})
+const bullConnection = new Redis(redisOptions);
 
-// 2. Connection specifically for BullMQ
-// We export the options because BullMQ prefers to manage its own connection lifecycle
-export const bullConnection = new Redis(redisOptions);
+redis.on("error", () => {
+  console.error("❌ Redis app connection failed");
+  process.exit(1);
+});
 
-bullConnection.on("connect", () => {
-  console.log("✅ Bull redis connection ready");
-})
+bullConnection.on("error", () => {
+  console.error("❌ Redis app connection failed");
+  process.exit(1);
+});
+
+export { redis, bullConnection };
