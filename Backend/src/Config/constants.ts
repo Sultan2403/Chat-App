93548308dad2a26a6bf47a3c8765fccc8ebdf67c
Sultan2403 <sultanremilekun@gmail.com>
2026-05-14 00:09:19 +0100

    added message events

--- Backend/src/Config/constants.ts
+++ Backend/src/Config/constants.ts
@@ -9,3 +9,10 @@ export const SOCKET_EVENTS = {
   CONNECTION: "connection",
   DISCONNECT: "disconnect",
 } as const;
+
+export const MESSAGE_EVENTS = {
+  NEW_MESSAGE: "new_message",
+  MESSAGE_READ: "message_read",
+  MESSAGE_DELIVERED: "message_delivered",
+} as const;
+
