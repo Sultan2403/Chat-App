87f3ff26eef6d45a0ba95f7e186b69c4b4321bd1
Sultan2403 <sultanremilekun@gmail.com>
2026-05-22 01:39:37 +0100

    removed unused stuff again

--- Backend/src/Routers/users.routes.ts
+++ Backend/src/Routers/users.routes.ts
@@ -1,25 +1,14 @@
-import validate from "express-zod-safe";
 import { Router } from "express";
-import { z } from "zod";
 
 const router = Router();
 
-// This can be deleted as it has been confirmed that the lib works as intended
-router.post(
-  "/",
-  validate({
-    body: z.object({
-      username: z.string().min(3, "Too short!"),
-      age: z.number().max(20, "Too old!").optional(),
-    }),
-  }),
-  (req, res) => {
-    // If it gets here, req.body.username is guaranteed to be a string
-    res.status(200).json({
-      message: "Validation passed!",
-      data: req.body,
+router.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({
+      success: true,
+      message: "There's nothing here yet, check back later :)",
     });
-  },
-);
+});
 
 export default router;
