63c15e48402d48f6ff1a7c77ebc94c372e588aa5
Sultan2403 <sultanremilekun@gmail.com>
2026-06-01 19:14:05 +0100

    update to js doc

--- Backend/src/Utils/utils.ts
+++ Backend/src/Utils/utils.ts
@@ -1,13 +1,12 @@
 import { z } from "zod";
 import { Types } from "mongoose";
 
-// Reusable validator that leverages Mongoose's native verification
-
 /**
+ * Reusable validator that leverages Mongoose's native verification
  * this mirrors what a mongoose ObjectId looks like, and uses mongoose's built in validation to ensure it's valid
  * @returns a zod schema that validates a string as a valid mongoose ObjectId
  */
 export const zObjectId = () =>
   z.string().refine((val) => Types.ObjectId.isValid(val), {
     message: "Invalid Mongoose ObjectId",
-});
+  });
