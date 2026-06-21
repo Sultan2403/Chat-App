9d74c97a5da82b20ed3638191000fd042a6d8314
Sultan2403 <sultanremilekun@gmail.com>
2026-06-22 00:25:27 +0100

    clerk init

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,9 +5,9 @@ import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/react";
 
 createRoot(document.getElementById("root")).render(
-  <ClerkProvider>
-    <StrictMode>
-      <App />
+
+    <StrictMode>  <ClerkProvider>
+      <App /></ClerkProvider>
     </StrictMode>
-  </ClerkProvider>,
+  ,
 );
