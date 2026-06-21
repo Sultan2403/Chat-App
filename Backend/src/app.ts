8fd3862cb988cd35eeadcaf5fcba1a95dced27cb
Sultan2403 <sultanremilekun@gmail.com>
2026-06-22 00:44:01 +0100

    clerk init finally done

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,6 +3,12 @@ import express, { Request, Response } from "express";
 
 // Middlewares
 import cors from "cors";
+import {
+  clerkMiddleware,
+  requireAuth,
+  getAuth,
+  clerkClient,
+} from "@clerk/express";
 
 // Routers
 import userRouter from "./Routers/users.routes";
@@ -16,9 +22,10 @@ app.use(
   }),
 );
 
+app.use(clerkMiddleware());
+
 app.use(express.json());
 
-// app.use(authMiddleware);
 app.use("/users", userRouter);
 
 // Routes
@@ -30,6 +37,20 @@ app.get("/", (req: Request, res: Response) => {
 
 app.get("/health", (req: Request, res: Response) => {
   res.status(200).json({ success: true, message: "Server says heyyy :)" });
+});
+
+app.get("/protected", requireAuth(), async (req, res) => {
+  // Use `getAuth()` to get the user's `userId`
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  // Use the `getUser()` method to get the user's User object
+  const user = await clerkClient.users.getUser(userId!);
+
+  return res.json({ user });
 });
 
 export default app;
