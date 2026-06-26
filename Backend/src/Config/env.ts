f7402f857006e2c855fbb52ce05c1f3cc31a5942
Sultan2403 <sultanremilekun@gmail.com>
2026-06-26 23:47:49 +0100

    updated env config

--- Backend/src/Config/env.ts
+++ Backend/src/Config/env.ts
@@ -1,18 +1,19 @@
 import { z } from "zod";
 import "dotenv/config";
 
-const envSchema = z
-  .object({
-    PORT: z.string().default("5000").transform(Number),
-    MONGO_DB_URI: z.string().min(1, "MongoDB URI is required"), // Safer than .url()
-    REDIS_URL: z.url().optional(), // Make optional if you use HOST/PORT
-    REDIS_HOST: z.string().default("localhost"),
-    // Handle string input and transform to number for the app
-    REDIS_PORT: z.string().default("6379").transform(Number),
-    NODE_ENV: z
-      .enum(["development", "production", "test"])
-      .default("development"),
-  });
+const envSchema = z.object({
+  PORT: z.string().default("5000").transform(Number),
+  MONGO_DB_URI: z.string().min(5, "MongoDB URI is required"), // Safer than .url()
+  REDIS_URL: z.url().optional(), // Make optional if you use HOST/PORT
+  REDIS_HOST: z.string().default("localhost"),
+  // Handle string input and transform to number for the app
+  REDIS_PORT: z.string().default("6379").transform(Number),
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+  CLERK_PUBLISHABLE_KEY: z.string().min(5, "Clerk publishable key missing"),
+  CLERK_SECRET_KEY: z.string().min(5, "Clerk secret key missing"),
+});
 
 const validateEnv = () => {
   const result = envSchema.safeParse(process.env);
