eaa9ab88c4d81bb05a1351ef4d9cdb012edc9810
Sultan2403 <sultanremilekun@gmail.com>
2026-06-30 18:17:37 +0100

    message service init... kinda

--- Backend/src/Services/message.service.ts
+++ Backend/src/Services/message.service.ts
@@ -1,5 +1,10 @@
-export const handleNewMessage = async () => {
-  // Prob push the saving to db to a queue to keep ui instant...
+import { Message } from "../Types/message.types";
 
-  
+export const handleNewMessage = async (message: Message) => {
+  // Prob push the saving to db to a queue to keep ui instant...
+  // await createMessageStoreJob(message)
+  // await storeToRedis(message)
+
+
+  // Idk if it should be returning something... Well we'll figure it out in the future
 };
